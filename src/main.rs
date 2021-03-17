c7a92b904609565f571b9a53a40771436db4ad9d
fabien.giannesini <fabien.giannesini@gmail.com>
2021-03-17 21:22:27 +0100

    get fizz on value 3 : red

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
 }
 
+fn fizzbuzz(value : i32) -> String {
+    "".to_string()
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::fizzbuzz;
 
+    #[test]
+    fn should_get_fizz_if_value_is_3() {
+        assert_eq!("fizz", fizzbuzz(3))
+    }
 }
