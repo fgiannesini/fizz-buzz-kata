86ad1c39e23d70104791ffd3ad265e6a638d54e3
fabien.giannesini <fabien.giannesini@gmail.com>
2021-03-17 21:23:31 +0100

    get empty on value 1 : red

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,9 @@ mod tests {
     fn should_get_fizz_if_value_is_3() {
         assert_eq!("fizz", fizzbuzz(3))
     }
+
+    #[test]
+    fn should_get_empty_if_value_is_1() {
+        assert_eq!("", fizzbuzz(1))
+    }
 }
